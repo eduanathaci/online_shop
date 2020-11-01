047e5e221d745bc870274bd24f14ba0578519f2a
Eduane Thaci <eduanath@gmail.com>
2020-11-01 16:11:34 +0100

    add simple footer

--- browser/src/components/Footer.js
+++ browser/src/components/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
   return (
-    <div>
-      footer
-    </div>
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">
+            Copyright &copy; Fashion Online Shop
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   )
 }
 
