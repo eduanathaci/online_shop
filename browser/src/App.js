491efa910e1da1a5ba49e5c9d2e71c234886e07a
Eduane Thaci <eduanath@gmail.com>
2020-11-01 15:59:00 +0100

    initial Footer and Header components

--- browser/src/App.js
+++ browser/src/App.js
@@ -1,6 +1,13 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 const App = () => {
   return (
-    <h1>Hello, here will be an awesome fashion online shop</h1>
+    <>
+      <Header />
+      <h1>Hello, here will be an awesome fashion online shop</h1>
+      <Footer />
+    </>
   );
 }
 
